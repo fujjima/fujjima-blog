440a87cf6e53c82105750e4d4bf600a2495b6cdd
katsuimasato <masato.lond2@outlook.jp>
2021-03-07 16:17:14 +0900

    add admin root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
+    root to: 'articles#index'
     resources :articles
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
