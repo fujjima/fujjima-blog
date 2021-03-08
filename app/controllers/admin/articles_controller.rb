a9381584659596622ac7da3f5b844c4924d5eae1
katsuimasato <masato.lond2@outlook.jp>
2021-03-08 23:45:12 +0900

    edit delete icon

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,5 +1,26 @@
 class Admin::ArticlesController < ApplicationController
+  before_action :set_article, only: %w[edit update destroy]
   def index
     @articles = Article.all
+  end
+
+  def edit; end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :status)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
