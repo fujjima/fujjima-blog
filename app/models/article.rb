56cecc4990b1b3a6a3c5ef8ae4c0f6949e9a9806
katsuimasato <masato.lond2@outlook.jp>
2021-03-18 15:02:50 +0900

    update published_at

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,14 @@
 class Article < ApplicationRecord
-  # 公開日時：publishedがfalse→trueになった日に変更される
   validates :published, inclusion: [true, false]
   validates :slug, presence: true
+
+  before_save :update_published_at, if: :will_save_change_to_published?
+
+  private
+
+  def update_published_at
+    return if published_was
+
+    assign_attributes(published_at: Time.now)
+  end
 end
