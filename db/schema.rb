e6fdbb77a41ad58f72e80b4f060937ac5f377bec
katsuimasato <masato.lond2@outlook.jp>
2021-03-18 16:41:08 +0900

    migrate category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_051320) do
+ActiveRecord::Schema.define(version: 2021_03_18_073148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_03_18_051320) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "published_at"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
