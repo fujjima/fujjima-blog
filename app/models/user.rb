0692fb6ebb109141a38ba11469cab677cec92ed5
katsuimasato <masato.lond2@outlook.jp>
2021-04-14 23:18:08 +0900

    add user to reset_password_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   enum role: { general: 0, admin: 1 }
   validates :password, length: { minimum: 6 }
+  # 間違いなくuserは一人しかいない想定だけど、今後ユーザーを増やす可能性は0ではないため一応uniqueness付けとく
+  validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
   validate :check_if_user_only, on: :create
 
   def check_if_user_only
