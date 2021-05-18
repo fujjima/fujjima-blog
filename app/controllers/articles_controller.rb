b306681e7dd60d45e7262871134b2506fb786fbc
katsuimasato <masato.lond2@outlook.jp>
2021-05-18 22:23:10 +0900

    render index for archives and tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,33 @@
 class ArticlesController < ApplicationController
+  before_action :set_archives, only: %w[index archives tags]
+
+
   def index
     # TODO: 1画面に6〜7記事ずつ記載する
     @articles = Article.published.order(published_at: 'DESC')
-    @archives = Article.published
-                       .group_by_yearly
-                       .map { |year, articles| { year: year, total: articles.count, months: aggregate_by_month(articles) } }
-
   end
 
   def archives
-    @archives = if params[:year] && params[:month]
+    @articles = if params[:year] && params[:month]
                   Article.published.get_by_month(params[:year], params[:month])
                 elsif params[:year] && !params[:month]
                   Article.published.get_by_year(params[:year])
                 end
+
+    render :index
   end
 
   def tags
+    render :index
   end
 
   private
+
+  def set_archives
+    @archives = Article.published
+                       .group_by_yearly
+                       .map { |year, articles| { year: year, total: articles.count, months: aggregate_by_month(articles) } }
+  end
 
   def aggregate_by_month(articles)
     articles.group_by { |article| article.published_at.strftime('%Y/%m') }
