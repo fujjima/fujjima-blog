e4d484047bc4b595bf098ec66a364d5e2826edaa
katsuimasato <masato.lond2@outlook.jp>
2021-08-09 17:09:29 +0900

    fix index.scss to index

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -5,7 +5,6 @@
 
 // 目的：アプリ全体で読み込まれるjs,cssモジュールの読み込み
 require("@rails/ujs").start()
-// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -13,8 +12,6 @@ import '@fortawesome/fontawesome-free/js/all';
 import 'bootstrap';
 import 'admin-lte';
 import 'src/stylesheets/application';
-// import '../src/javascript/base';
-// import '../src/stylesheets/base'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
