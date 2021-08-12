f454c8aa9aab0ed9111ca7138bed20a04cc9e953
katsuimasato <masato.lond2@outlook.jp>
2021-08-12 23:27:52 +0900

    fix order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < GeneralController
   PER_PAGE = 7
 
   def index
-    @articles = paginate(Article.published.sort)
+    @articles = paginate(Article.published.sort_by(&:published_at).reverse!)
   end
 
   def archives
