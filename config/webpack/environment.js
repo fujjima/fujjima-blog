2f09d32ffa7fc906a55c0ec514799b17aaa4ea5f
katsuimasato <masato.lond2@outlook.jp>
2022-01-31 00:02:05 +0900

    swtich node-sass to dart-sass in webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,13 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
+
+const sassLoader = environment.loaders.get('sass')['use'].find(rule => rule['loader'] === 'sass-loader')
+sassLoader.options = {
+  ...sassLoader.options,
+  implementation: require('sass'),
+}
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
