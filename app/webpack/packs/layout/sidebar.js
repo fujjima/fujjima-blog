a2e09613d0740deb389a0957c4b20b040f4b13e8
fujjima <masato.lond2@outlook.jp>
2024-10-03 01:49:54 +0900

    webpack, webpacker系の削除 (#155)
    * webpackのconfig系の削除
    
    * gem及び設定系のファイルの削除
    
    * yarnに入れていたwebpacker系のモジュールの削除
    
    * 不要になったwebpack関連のコマンドを削除
    
    * 不要になったwebpack関連のファイルを削除

--- app/webpack/packs/layout/sidebar.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-  $('.show_button, .hide_button').click(function () {
-    $(this).toggleClass('hide');
-    $(this).next().is('span') ?
-      $(this).next().toggleClass('hide') :
-      $(this).prev().toggleClass('hide')
-  })
-});
