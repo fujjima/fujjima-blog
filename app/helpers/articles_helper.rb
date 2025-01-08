36441ae8e1c9cf8b7d469f843b38b2bb2e231ea0
katsuimasato <masato.lond2@outlook.jp>
2025-01-08 13:38:37 +0900

    twitterカードを表示するように設定

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,17 @@
 module ArticlesHelper
-  
-  def join_with_comma strs = []
+  META_TAG_TEXT_MAC_SIZE = 50
+
+  def join_with_comma(strs = [])
     return '' if strs.empty?
 
     strs.join(', ')
+  end
+
+  def extract_text_for_meta_tags(text = '')
+    extract_text = ActionView::Base.full_sanitizer
+                                   .sanitize(text)
+                                   .gsub(/[\r\n]/, '')
+
+    truncate(extract_text, length: META_TAG_TEXT_MAC_SIZE)
   end
 end
