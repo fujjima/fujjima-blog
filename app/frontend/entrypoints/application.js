839298f40ae33381cfa5a238423d67fb6d7f1410
fujjima <masato.lond2@outlook.jp>
2025-01-08 15:25:35 +0900

    Twitterカード表示用のメタタグの設定 (#166)
    * meta-tagsインストール
    
    * faviconの変更
    
    * twitterカードを表示するように設定

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -28,8 +28,6 @@
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
 
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
@@ -42,3 +40,6 @@ window.$ = $;
 
 Rails.start()
 ActiveStorage.start()
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+  # rubocop:disable Metrics/MethodLength
+  def default_meta_tags
+    image_url = vite_asset_path('images/icon_800x800.png')
+    twitter_account = Rails.env.production? ? ENV['TWITTER_ACCOUNT'] : Rails.application.credentials.twitter_account
+
+    {
+      site: 'fujjimablog',
+      title: 'fujjimablog',
+      reverse: true,
+      charset: 'utf-8',
+      description: '記事の一覧にアクセス',
+      keywords: 'ブログ,技術系,Rails,React,服',
+      canonical: request.original_url,
+      separator: '|',
+      # icon: [
+      #   { href: image_url('logo.png') },
+      #   { href: image_url('top_image.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      # ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        # image:,# 配置するパスやファイル名によって変更する
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary',
+        site: twitter_account,
+        image: image_url
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 end
