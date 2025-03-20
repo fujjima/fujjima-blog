4d1386d583a8f632da9f1d03dba6a103754e9e6c
katsuimasato <masato.lond2@outlook.jp>
2025-03-21 04:03:29 +0900

    タグが空でもエラーが起きないように修正

--- app/controllers/concerns/taggable.rb
+++ app/controllers/concerns/taggable.rb
@@ -8,15 +8,15 @@ module Taggable
   end
 
   def load_tags
-      all_tags = Tag.all.to_a
-      # その他タグを配列の最後尾に並び直す
-      all_tags.partition{ |tag| tag.name != 'その他' }.flatten
+    all_tags = Tag.all.to_a
+    # その他タグを配列の最後尾に並び直す
+    all_tags.partition { |tag| tag.name != 'その他' }.flatten
   end
   memoize :load_tags
 
   # params内のタグ名は","区切りの文字列
   def tag_names_to_ary(str)
-    str.empty? ? [] : str.split(',')
+    str.present? ? str.split(',') : []
   end
 
   def find_or_create_tags_by_name(tag_name)
