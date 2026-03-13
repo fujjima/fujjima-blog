fb28e64b210283a14badf448fbf87a2253d2e7bd
katsuimasato <masato.lond2@outlook.jp>
2026-03-13 17:57:28 +0900

    sorceryの外部認証用のエンドポイントを追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -4,6 +4,11 @@ class Admin::UserSessionsController < AdminController
 
   def new; end
 
+  def oauth
+    # NOTE: 内部でsorceryが外部認証へのリダイレクトを行う
+    login_at(params[:provider])
+  end
+
   def create
     @user = login(params[:email], params[:password])
     # TODO: @userがいるか、メールの認証が完了しているかどうか
@@ -15,8 +20,35 @@ class Admin::UserSessionsController < AdminController
     end
   end
 
+  # NOTE: 外部認証からのコールバック用のエンドポイント。外部認証結果を元に、ユーザーのログイン処理を行う
+  def omniauth_callback
+    provider = auth_params[:provider]
+
+    # NOTE: login_fromの仕様について(https://github.com/Sorcery/sorcery?tab=readme-ov-file#external)
+    if (@user = login_from(provider))
+      redirect_to admin_dashboards_path, notice: "#{provider.titleize}アカウントでログインしました"
+    else
+      begin
+        @user = create_from(provider)
+
+        # NOTE: protect from session fixation attack
+        reset_session
+        auto_login(@user)
+        redirect_to admin_dashboards_path, notice: "#{provider.titleize}アカウントを追加し、ログインしました。"
+      rescue StandardError
+        redirect_to root_path, alert: "#{provider.titleize}アカウントでのログインに失敗しました"
+      end
+    end
+  end
+
   def destroy
     logout
     redirect_to admin_login_path, notice: 'sucessed to logout'
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,51 @@
 Rails.application.routes.draw do
+  # 管理画面
   namespace :admin do
-    get '/login', to: 'user_sessions#new'
-    post '/login', to: 'user_sessions#create'
-    post '/logout', to: 'user_sessions#destroy'
-    get '/register', to: 'users#new'
+    # 認証関連
+    get  '/login',    to: 'user_sessions#new'
+    post '/login',    to: 'user_sessions#create'
+    post '/logout',   to: 'user_sessions#destroy'
+
+    # OAuth
+    get  'auth/:provider/callback', to: 'user_sessions#omniauth_callback'
+    get  'auth/failure',            to: redirect('/admin/login')
+    get  'auth/:provider',          to: 'user_sessions#oauth', as: :auth_at_provider
+
+    # ユーザー登録
+    get  '/register', to: 'users#new'
     post '/register', to: 'users#create'
+
     get '/dashboards', to: 'dashboards#index'
+
     resources :users do
       get :activate, on: :member
     end
-    resources :reset_passwords, only: %w[new create edit update]
+
+    resources :reset_passwords, only: %i[new create edit update]
+
     resources :articles do
       collection do
         post :upload_image
       end
     end
-    resources :tags, only: %w[index] do
+
+    resources :tags, only: %i[index] do
       post :update, on: :collection
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: 'articles#index'
   get '/question', to: 'articles#question'
   get '/:slug', to: 'articles#show', as: :article
 
-  # TODO: 2024/2のようなルーティングではなく、クエリパラメータで分岐させるか、archivesというリソースを作る
-  get '/archives/:year', to: 'articles#archives', constraints: { year: /\d{4}/ }, as: 'archives_by_year'
+  # TODO: クエリパラメータ方式か archives リソースへの変更を検討
+  archive_constraints = { year: /\d{4}/, month: /\d{1,2}/ }.freeze
+  get '/archives/:year',        to: 'articles#archives', constraints: archive_constraints, as: 'archives_by_year'
+  get '/archives/:year/:month', to: 'articles#archives', constraints: archive_constraints, as: 'archives_by_month'
 
-  get '/archives/:year/:month', to: 'articles#archives', constraints: {
-    year: /\d{4}/,
-    month: /\d{1,2}/
-  }, as: 'archives_by_month'
-
-  # タグの詳細画面はなく、記事のタグによる絞り込み機能のみがある
+  # タグによる記事絞り込み（タグ詳細画面はなし）
   get '/tags/:tag_name', to: 'articles#tags', as: 'article_tags'
+  resources :tags, only: %i[index]
 
-  # タグに関する画面はタグ一覧画面のみ
-  resources :tags, only: %w[index]
-
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: '/letter_opener'
-  end
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
