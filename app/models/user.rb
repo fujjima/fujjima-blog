4de45972c5c88fc2d90b347ec1c5507376d3f16f
Masato Katsui <masato.lond2@outlook.jp>
2026-08-09 21:40:40 +0900

    google認証できるようにする (#184)
    * omuniauthとtailwindのインストール
    
    * クレデンシャルの追加
    
    * omniauth-rails_csrf_protectionのインストール
    
    * authenticationsテーブルの追加
    
    * 不要となったdotenv-railsの削除
    
    * omniauth関連のモデルの追加
    
    * vite用のtailwindをインストールし、不要なgem等を削除
    
    * 設定系の修正
    
    * sorceryの外部認証用のエンドポイントを追加
    
    * googleログイン用のボタンを追加
    
    * uidとproviderに関するユニーク制約の追加
    
    * 特定アカウント以外のgoogle認証の禁止

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  enum role: { general: 0, admin: 1 }
 
-  # confirmation: true, xxx_confirmationと値が一致するか検証する
+  # NOTE: confirmation: true, xxx_confirmationと値が一致するか検証する
   validates :password, confirmation: true, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  # userは一人しかいない想定だが、今後ユーザーを増やす可能性は0ではないため一応uniqueness付けとく
+  # NOTE: userは一人しかいない想定だが、今後ユーザーを増やす可能性は0ではないため一応uniqueness付けとく
   validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
   validate :check_if_user_only, on: :create
+
+  has_many :authentications, dependent: :destroy
+
+  accepts_nested_attributes_for :authentications
+
+  enum role: { general: 0, admin: 1 }
 
   def check_if_user_only
     errors.add(:user, '既に上限ユーザー数に達しています') if User.count >= 1
